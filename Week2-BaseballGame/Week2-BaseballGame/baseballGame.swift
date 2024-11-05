90ed646281d5e91881a127d2120f3a03458fd581
chhue <chhue96@gmail.com>
2024-11-05 22:42:01 +0900

    [Feat] 입력값 올바른지 확인후 스트라이크 / 볼 개수 카운트하여 출력
    입력값이 올바르지 않은 경우 Error로 정의
    입력값을 guard문으로 나누어 Error 던져주는 메소드 정의
    do문에서 해당 Error 받아 올바르게 작동되는 경우 Strike/Ball 개수 파악, catch문에서 Error에 따른 오류메시지 출력

--- Week2-BaseballGame/Week2-BaseballGame/baseballGame.swift
+++ Week2-BaseballGame/Week2-BaseballGame/baseballGame.swift
@@ -5,35 +5,152 @@
 //  Created by CHYUN on 11/5/24.
 //
 
-
 class BaseballGame {
     
     enum GameMessage: String {
-        case start = "Game Start",
-             input = "please input your number",
-             end = "Game End",
-             strike = "Strike",
-             ball = "Ball",
-             out = "Out"
+        case gemeStartMessage = "Game Start",
+             requireInputMessage = "Please enter the numbers for each question mark in order, then press Enter.",
+             gameEndMessage = "Game End",
+             strikeMessage = "Strike",
+             ballMessage = "Ball",
+             outMessage = "Out"
+        
+        static func printGuidanceMessage(message: GameMessage) {
+            print(message.rawValue)
+        }
     }
-
+    
+    enum InputError: Error {
+        case inputCountError,
+             invalidInputError,
+             zeroInputError,
+             duplicateValueError,
+             unknownError
+        
+        static func printErrorMessage(error: InputError) {
+            
+            var message = ""
+            
+            switch error {
+            case inputCountError:
+                message = "The number of inputs is insufficient."
+            case invalidInputError:
+                message = "Invalid input."
+            case zeroInputError:
+                message = "Zero is not allowed."
+            case duplicateValueError:
+                message = "There are duplicate values."
+            case unknownError:
+                message = "There are unknown errors."
+            }
+            
+            print(message)
+        }
+    }
     
     private var answer: [Int] = []
-    private let numberOfAnsewer = 3
+    private let numberOfAnswer = 3
+    private var playerInput: [Int?] = []
+    private let rangeOfAnswer = 1...9
     
+
     func startNewSet() {
         
-        /**
-         * answer의 count가 numberOfAnswer가 될 때까지 0에서 9이내의 랜덤 Int 추출 반복
-         * 동일한 수가 없을 때만 append
-         **/
-        while answer.count < numberOfAnsewer {
-            let randomNumber = Int.random(in: 0...9)
+        var questionBoxes = ""
+        
+        while answer.count < numberOfAnswer {
+            let randomNumber = Int.random(in: rangeOfAnswer)
             if !answer.contains(randomNumber) {
                 answer.append(randomNumber)
+                questionBoxes = questionBoxes + "[ ? ] "
             }
         }
-        print(GameMessage.start.rawValue)
+        
+        GameMessage.printGuidanceMessage(message: .gemeStartMessage)
+        print(questionBoxes)
+    
         print(answer) //test
     }
+    
+    func getPlayerInput() {
+        
+        GameMessage.printGuidanceMessage(message: .requireInputMessage)
+        
+        /** 값 비교 전 체크 필요
+         * [ 해결 가능 - 제일 먼저 체크 ]
+         * 1. 공백 문자 삽입 여부
+         * -------------> 공백 제외 후 비교
+         *
+         * [ 입력 개수 파악 ]
+         * 2. 입력 값 개수가 numberOfAnswer보다 부족 (입력 값 없을 경우 포함)
+         *
+         * [ 올바르지 않은입력 값 파악 ]
+         * 3. Int 값 외
+         * 4. 0 입력
+         * 5. 중복 숫자 입력
+         * -------------> 다시 입력 받기
+         **/
+        
+        var tempInput = readLine()!
+        tempInput.replace(" " , with: "")
+        
+        do {
+            // 이상 없이 정상 진행될 경우
+            playerInput = try checkError(playerInput: tempInput)
+            
+            var strikeCount = 0
+            var ballCount = 0
+            for index in playerInput.startIndex...playerInput.count - 1 {
+                answer[index] == playerInput[index]! ? strikeCount += 1 : nil
+                answer.contains(playerInput[index]!) && answer[index] != playerInput[index]! ? ballCount += 1 : nil
+            }
+            
+            print("\(strikeCount) \(GameMessage.strikeMessage.rawValue) \(ballCount) \(GameMessage.ballMessage.rawValue)")
+            
+            
+        } catch InputError.inputCountError {
+            InputError.printErrorMessage(error: InputError.inputCountError)
+            
+        } catch InputError.invalidInputError {
+            InputError.printErrorMessage(error: InputError.invalidInputError)
+            
+        } catch InputError.zeroInputError {
+            InputError.printErrorMessage(error: InputError.zeroInputError)
+            
+        } catch InputError.duplicateValueError {
+            InputError.printErrorMessage(error: InputError.duplicateValueError)
+            
+        } catch {
+            InputError.printErrorMessage(error: InputError.unknownError)
+        }
+        
+
+    }
+    
+    private func checkError(playerInput: String) throws -> [Int?] {
+        
+        let inputAsStringArray = playerInput.split(separator: "")
+        
+        guard inputAsStringArray.count == numberOfAnswer else {
+            throw InputError.inputCountError
+        }
+        
+        let inputAsIntArray = inputAsStringArray.map({ Int($0) })
+        
+        guard !inputAsIntArray.contains(nil) else {
+            throw InputError.invalidInputError
+        }
+        
+        guard !inputAsIntArray.contains(0) else {
+            throw InputError.zeroInputError
+        }
+        
+        guard Set(inputAsIntArray).count == inputAsIntArray.count else {
+            throw InputError.duplicateValueError
+        }
+        
+        return inputAsIntArray
+        
+    }
+    
 }
--- Week2-BaseballGame/Week2-BaseballGame/main.swift
+++ Week2-BaseballGame/Week2-BaseballGame/main.swift
@@ -1,5 +1,10 @@
 import Foundation
 
 // 게임 시작
-BaseballGame().startNewSet()
+//let test = [5,7,6,0,0]
+//print(BaseballGame().isThereDuplicateValue(array: test))
+let baseballGame = BaseballGame()
+baseballGame.startNewSet()
+baseballGame.getPlayerInput()
+
 
