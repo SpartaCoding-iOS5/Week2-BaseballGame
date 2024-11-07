0663a4f2d449bf776cf00bdf9b7f782a9dcb42a5
ericKwon95 <ericyong95@gmail.com>
2024-11-07 14:02:17 +0900

    refactor: 확장성 있는 방식으로 구조 변경
    - BaseballGame 상속받아 각각의 level마다 필요한 기능 오버라이드 또는 추가 구현할 수 있도록 변경
    - main에서 BaseballGame 갈아끼워 게임 수행

---
+++ Week2-BaseballGame/Week2-BaseballGame/Base/Game/BaseballGame.swift
@@ -0,0 +1,12 @@
+//
+//  BaseballGame.swift
+//  Week2-BaseballGame
+//
+//  Created by 권승용 on 11/7/24.
+//
+
+class BaseballGame {
+    func startGame(questioner: Player, answerer: Player) throws {
+        return
+    }
+}
