250b2319c6c04e97aef59151c70fe2d2b251f56a
ericKwon95 <ericyong95@gmail.com>
2024-11-07 10:17:54 +0900

    feat: Level1 기능구현

---
+++ Week2-BaseballGame/Week2-BaseballGame/Lv_1(필수구현)/Error/BaseballGameError.swift
@@ -0,0 +1,30 @@
+//
+//  BaseballGameError.swift
+//  Week2-BaseballGame
+//
+//  Created by 권승용 on 11/7/24.
+//
+
+import Foundation
+
+enum BaseballGameError {
+    enum InputError: LocalizedError {
+        case notString
+        case countNotMatching
+        case zeroDetected
+        case notInteger
+        
+        var errorDescription: String? {
+            switch self {
+            case .notString:
+                return "문자열 입력이 아닙니다."
+            case .countNotMatching:
+                return "입력 수의 갯수가 다릅니다."
+            case .zeroDetected:
+                return "0은 입력할 수 없습니다."
+            case .notInteger:
+                return "정수 입력이 아닙니다."
+            }
+        }
+    }
+}
