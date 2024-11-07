69a69996203b2140ad22997e6dbe025bc371b0e6
ericKwon95 <ericyong95@gmail.com>
2024-11-07 16:52:14 +0900

    refactor: 변경된 설계로 인해 startGame은 재사용하고 proceedGame / showRecord만 재정의하면 된다.

--- Week2-BaseballGame/Week2-BaseballGame/Lv_5(도전구현)/BaseballGameLevel5.swift
+++ Week2-BaseballGame/Week2-BaseballGame/Lv_5(도전구현)/BaseballGameLevel5.swift
@@ -6,31 +6,7 @@
 //
 
 class BaseballGameLevel5: BaseballGameLevel4 {
-    override func startGame(questioner: any Player, answerer: any Player) throws {
-        print("환영합니다! 원하시는 번호를 입력해주세요")
-        
-        while true {
-            print("1. 게임 시작하기 2. 게임 기록 보기 3. 종료하기")
-            
-            if let input = readLine(), let num = Int(input) {
-                switch num {
-                case 1:
-                    try proceedGame(questioner: questioner, answerer: answerer)
-                    
-                case 2:
-                    RecordManager.shared.printRecords()
-                    
-                case 3:
-                    break
-                    
-                default:
-                    break
-                }
-            }
-        }
-    }
-    
-    func proceedGame(questioner: Player, answerer: Player) throws {
+    override func proceedGame(questioner: Player, answerer: Player) throws {
         let question = try questioner.answer()
         var count = 1
         
@@ -47,5 +23,9 @@ class BaseballGameLevel5: BaseballGameLevel4 {
                 count += 1
             }
         }
+    }
+    
+    override func showRecord() {
+        RecordManager.shared.printRecords()
     }
 }
